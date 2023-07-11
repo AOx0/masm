139cacaedc5905360e4d2c134cade2dc98fddbf6
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-07-11 15:42:09 -0600

    Add Readme and rust-toolchain

---
+++ README.md
@@ -0,0 +1,78 @@
+# masm
+
+A simple assembler for x86_64 assembly that generates relocatable ELF files.
+
+## Install
+
+### Nightly Rust
+
+To install/compile the assembler you need to have rust installed. You can install rust from [here](https://www.rust-lang.org/tools/install).
+The project uses the nightly toolchain, so you may need to install it with:
+
+```bash
+rustup toolchain install nightly
+```
+
+### Install with Cargo
+
+Clone the repository and run
+```bash
+cargo install --path ./path/to/masm
+```
+
+Or directly install it from git:
+```bash
+cargo install --git https://github.com/AOx0/masm
+```
+
+## Usage 
+
+To compile an assembly file run:
+
+```bash
+masm -p <input> [-o <output>]
+```
+
+This generates a relocatable ELF file. To link (on linux) it run:
+
+```bash
+ld -o main <output>
+```
+
+As with any linkable file, you may need to specify the linker library paths and nexxesary arguments to link external symbols.
+
+## Example
+
+With the assembly file `main.s`:
+
+```asm
+end:
+    mov  rax, 0x3c  ;; sys_exit
+    mov  rdi, 10    ;; arg0 = 10
+    syscall         ;; call
+
+main:
+    call end        ;; call end
+```
+
+Here the `main` function is the entrypoint, the entrypoint routine should always be named main or _start
+
+WWe compile the file with:
+
+```bash
+masm -p main.s -o main.o
+```
+
+And link it with:
+
+```bash
+ld -o main main.o
+```
+
+Now we can run the program with:
+
+```bash
+./main
+```
+
+As expected,it exits with the status code 10
--- test.asm
+++ test.asm
@@ -1,52 +1,8 @@
-; Termina el programa con número de estado 10
-
-msg db 'H', "ola", 0xA, 0
-msg2 db -1
-
 end:
-mov rax, rdi
-mov edi, eax
-mov  rax, [rcx] ;; sys_exit
-mov  [rcx], rax ;; sys_exit
-mov  rax, [rsi + rdi * 0x1 + 4] ;; sys_exit
-mov  [rsi + rdi * 0x1 + 4], rax ;; sys_exit
-mov  rax, [rsi + rdi + 4] ;; sys_exit
-mov  rax, [rcx + rdi + 4] ;; sys_exit
-mov  rax, [rcx + rdi + 4] ;; sys_exit
-mov  rax, [rsi + rdi + 0x0] ;; sys_exit
-mov  rax, [rsi + rdi + 0xA] ;; sys_exit
-mov  eax, [rsi + rdi + 0x0] ;; sys_exit
-mov  eax, [rsi + rdi + 0xA] ;; sys_exit
-mov  ax, [rsi + rdi + 0x0] ;; sys_exit
-mov  ax, [rsi + rdi + 0xA] ;; sys_exit
-mov  rcx, [rsi + rdi + 0x0] ;; sys_exit
-mov  rcx, [rsi + rdi + 0xA] ;; sys_exit
-mov  ecx, [rsi + rdi + 0x0] ;; sys_exit
-mov  ecx, [rsi + rdi + 0xA] ;; sys_exit
-mov  cx, [rsi + rdi * 2 + 0x0] ;; sys_exit
-mov  cx, [rsi + rdi * 2  + 0xA] ;; sys_exit
-mov  rax, [rsi + rdi* 2  + 0x0] ;; sys_exit
-mov  rax, [rsi + rdi* 2  + 0xA] ;; sys_exit
-mov  eax, [rsi + rdi* 2  + 0x0] ;; sys_exit
-mov  eax, [rsi + rdi* 2  + 0xA] ;; sys_exit
-mov  ax, [rsi + rdi* 2  + 0x0] ;; sys_exit
-mov  ax, [rsi + rdi* 2  + 0xA] ;; sys_exit
-mov  rcx, [rsi + rdi * 2 + 0x0] ;; sys_exit
-mov  rcx, [rsi + rdi* 2  + 0xA] ;; sys_exit
-mov  ecx, [rsi + rdi* 2  + 0x0] ;; sys_exit
-mov  ecx, [rsi + rdi* 2  + 0xA] ;; sys_exit
-mov  cx, [rsi + rdi + 0x0] ;; sys_exit
-mov  cx, [rsi + rdi + 0xA] ;; sys_exit
-mov  ax, 0x3   ;; sys_exit
-mov  rdi, 10   ;; arg0 = 10
-push rax
-push rsi
-push 4
-push -1
-pop rdx
-syscall        ;; call
-ret
-    
+    mov  rax, 0x3c  ;; sys_exit
+    mov  rdi, 10   ;; arg0 = 10
+    syscall        ;; call
+    ret
 
 main:
-call end
+    call end        ;; call end
